218ed8754418cfae12a8878a601be467b97810a0
tanvi3110 <tanvishah3110@gmail.com>
2021-08-02 21:41:23 -0400

    Task: Part 11 completed.

---
+++ flask_session_tutorial/flask-session-tutorial/__init__.py
@@ -0,0 +1,40 @@
+"""Initialize app."""
+from flask import Flask
+from flask_login import LoginManager
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from ddtrace import patch_all
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+sess = Session()
+patch_all()
+
+
+def create_app():
+    """Construct the core flask_session_tutorial."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object("config.Config")
+
+    # Initialize Plugins
+    db.init_app(app)
+    login_manager.init_app(app)
+    sess.init_app(app)
+
+    with app.app_context():
+        from . import routes
+        from . import auth
+        from .assets import compile_static_assets, compile_auth_assets
+
+        app.register_blueprint(routes.main_bp)
+        app.register_blueprint(auth.auth_bp)
+
+        # Create static asset bundles
+        compile_static_assets(app)
+        compile_auth_assets(app)
+
+        # Create Database Models
+        db.create_all()
+
+        return app
---
+++ flask_session_tutorial/flask-session-tutorial/config.py
@@ -0,0 +1,37 @@
+"""App configuration."""
+from os import environ, path
+
+import redis
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    """Set Flask configuration variables from .env file."""
+
+    # General Config
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = environ.get("SECRET_KEY")
+
+    # Flask-Session
+    REDIS_URI = environ.get("SESSION_REDIS")
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.from_url(REDIS_URI)
+
+    # Flask-Assets
+    LESS_BIN = environ.get("LESS_BIN")
+    ASSETS_DEBUG = False
+    LESS_RUN_IN_DEBUG = False
+
+    # Static Assets
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = True
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
