c317fe0e910210a46aa6aa0b52193f7bb2406026
tanvi3110 <tanvishah3110@gmail.com>
2021-08-02 22:31:55 -0400

    Task: Part 9 completed.

--- flask_sqlalchemy_tutorial/flask-sqlalchemy-tutorial/models.py
+++ flask_sqlalchemy_tutorial/flask-sqlalchemy-tutorial/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
 
     __tablename__ = "flasksqlalchemy-tutorial-users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=False, unique=True, nullable=False)
+    username = db.Column(db.String(65), index=False, unique=True, nullable=False)
     email = db.Column(db.String(80), index=True, unique=True, nullable=False)
     created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
     bio = db.Column(db.Text, index=False, unique=False, nullable=True)
